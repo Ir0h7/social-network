43d36c7095c614178ad3689d12f6a512c0c5068d
dstroshkov <stroshkov.1977@icloud.com>
2023-11-07 00:01:25 +0300

    changelog: - add logs - change __init__.py: add package logging - in models.py: create table "followers", change class User: add relationship "followed", methods: follow(), unfollow(), is_following() - in templates: update user.html - in forms.py: update EditProfileForm - in views_all.py: add routes: follow(), unfollow() - create few migrations - create dir tests: __init__.py, test_users.py - update requirements.txt - change config.py: add package dotenv - add .gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,32 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://postgres:20453843@localhost:5433/social_network"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 
-from app import views_all, models, errors
+if not app.debug:
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler(
+        "logs/social-network.log", maxBytes=10240, backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Social network startup")
+
+from app import views_all, models, errors, tests
--- config.py
+++ config.py
@@ -1,18 +1,9 @@
 import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    ) or "postgresql://" + os.path.join(basedir, "app.db")
+    SECRET_KEY = os.getenv("SECRET_KEY", "you-will-never-guess")
+    load_dotenv()
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = ["your-email@example.com"]
