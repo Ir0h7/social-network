ddbf6d3add6b0ec344c617edb60887c77b82ac73
dstroshkov <stroshkov.1977@icloud.com>
2023-11-08 20:17:48 +0300

    changelog: - add the ability to reset the password by sending a message to a user email - add improved time handling - improve the design of the web application - add email.py - change __init__.py: add imports: Mail, Bootstrap, Moment - in models.py: change class User: add methods: get_reset_password(), verify_reset_password_token() - in templates: update: user.html, register.html, login.html, index.html, edit_profile.html, base.html, _post.html, 404.html, 500.html add dir templates/email: reset_password.html, reset_password_request.html, reset_password.txt - in forms.py: add PostFrom, ResetPasswordRequestForm, ResetPasswordForm, EmptyForm - in views_all.py: add routes: reset_password_request(), reset_password(), explore(), change: index(), user(), - update requirements.txt - change config.py: add mail settings

--- app/views_all.py
+++ app/views_all.py
@@ -1,25 +1,46 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import (
+    LoginForm,
+    RegistrationForm,
+    EditProfileForm,
+    PostForm,
+    ResetPasswordRequestForm,
+    ResetPasswordForm,
+    EmptyForm,
+)
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from urllib.parse import urlsplit
 from datetime import datetime
+from app.email import send_password_reset_email
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
-    posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
-        {
-            "author": {"username": "Ипполит"},
-            "body": "Какая гадость эта ваша заливная рыба!!",
-        },
-    ]
-    return render_template("index.html", title="Home Page", posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post is now live!")
+        return redirect(url_for("index"))
+    page = request.args.get("page", 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
+    )
+    next_url = url_for("index", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        "index.html",
+        title="Home",
+        form=form,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -38,6 +59,38 @@ def login():
             next_page = url_for("index")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/reset_password_request", methods=["GET", "POST"])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash("Check your email for the instructions to reset your password")
+        return redirect(url_for("login"))
+    return render_template(
+        "reset_password_request.html", title="Reset Password", form=form
+    )
+
+
+@app.route("/reset_password/<token>", methods=["GET", "POST"])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for("index"))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash("Your password has been reset.")
+        return redirect(url_for("login"))
+    return render_template("reset_password.html", form=form)
 
 
 @app.route("/logout")
@@ -65,11 +118,29 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {"author": user, "body": "Test post #1"},
-        {"author": user, "body": "Test post #2"},
-    ]
-    return render_template("user.html", user=user, posts=posts)
+    page = request.args.get("page", 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
+    )
+    next_url = (
+        url_for("user", username=user.username, page=posts.next_num)
+        if posts.has_next
+        else None
+    )
+    prev_url = (
+        url_for("user", username=user.username, page=posts.prev_num)
+        if posts.has_prev
+        else None
+    )
+    form = EmptyForm()
+    return render_template(
+        "user.html",
+        user=user,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+        form=form,
+    )
 
 
 @app.before_request
@@ -125,3 +196,21 @@ def unfollow(username):
     db.session.commit()
     flash("You are not following {}.".format(username))
     return redirect(url_for("user", username=username))
+
+
+@app.route("/explore")
+@login_required
+def explore():
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
+    )
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
